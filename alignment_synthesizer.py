a250d20143d7fd8ed901479e972934c99cc1d7c9
Joshua Efiong <joshefiong@gmail.com>
2019-02-04 00:27:45 +0000

    Fix alignment synth by renaming variables

--- alignment_synthesizer.py
+++ alignment_synthesizer.py
@@ -3,24 +3,24 @@ import tensorflow as tf
 from hparams import hparams
 from models import create_model
 from text import text_to_sequence
+from util.rename import rename_scope
 
 
 class AlignmentSynthesizer:
-  def load(self, checkpoint_path, model_name='tacotron_pml'):
+  def load(self, checkpoint_path, model_name='tacotron_pml', original_scope_name='model', scope_name='alignment_model'):
     print('Constructing model: %s' % model_name)
     inputs = tf.placeholder(tf.int32, [1, None], 'inputs')
     input_lengths = tf.placeholder(tf.int32, [1], 'input_lengths')
 
-    with tf.variable_scope('alignment_model') as scope:
+    with tf.variable_scope(scope_name) as scope:
       self.model = create_model(model_name, hparams)
       self.model.initialize(inputs, input_lengths)
       self.alignment = self.model.alignments[0]
 
     print('Loading checkpoint: %s' % checkpoint_path)
     self.session = tf.Session()
+    rename_scope(self.session, checkpoint_path, original_scope_name, scope_name)
     self.session.run(tf.global_variables_initializer())
-    saver = tf.train.Saver()
-    saver.restore(self.session, checkpoint_path)
 
   def synthesize(self, text):
     cleaner_names = [x.strip() for x in hparams.cleaners.split(',')]
